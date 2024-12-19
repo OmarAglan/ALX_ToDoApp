500c558b6b58e56b01a886113413e19d657254c1
Omar Aglan <omar.aglan91@yahoo.com>
2024-12-20 00:55:34 +0200

    Basic Layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,30 @@
 import React from 'react';
+import './App.css';
 
 const App = () => {
     return (
-        <div>
+        <div className='app'>
             <header className='app_header_sec'>
                 <h1>React App</h1>
             </header>
-            <p>
-                This is a simple React app.
-            </p>
+            <main className='app_main_sec'>
+                <section className='task_column_part'>
+                    section 1 - Backlog
+                </section>
+                <section className='task_column_part'>
+                    section 2 - In Progress
+                </section>
+                <section className='task_column_part'>
+                    section 3 - Late Task
+                </section>
+                <section className='task_column_part'>
+                    section 4 - Done
+                </section>
+                <section className='task_column_part'>
+                    section 5 - Archive
+                </section>
+            </main>
+
         </div>
     );
 }
