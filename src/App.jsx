6d5ce148f7cccb8234d65a65574af3fbbae96b86
Omar Aglan <omar.aglan91@yahoo.com>
2024-12-20 00:18:04 +0200

    Start ALX Final Project ToDo App

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const App = () => {
+    return (
+        <div>
+            <header className='app_header_sec'>
+                <h1>React App</h1>
+            </header>
+        </div>
+    );
+}
+
+export default App;
