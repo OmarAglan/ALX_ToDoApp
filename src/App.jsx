a7dae5ba075cb4e8d11fb6b07afabee35ad335b1
Omar Aglan <omar.aglan91@yahoo.com>
2024-12-24 07:41:19 +0200

    Taskform Input part1

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,25 @@
 import React from 'react';
 import './App.css';
+import TaskFormInput from "../component/TaskFormInput.jsx";
 
 const App = () => {
     return (
         <div className='app'>
-            <header className='app_header_sec'>
-                <h1>React App</h1>
-            </header>
-            <main className='app_main_sec'>
-                <section className='task_column_part'>
+            <TaskFormInput></TaskFormInput>
+            <main className={"app_main_sec"}>
+                <section className={"task_column_part"}>
                     section 1 - Backlog
                 </section>
-                <section className='task_column_part'>
+                <section className={"task_column_part"}>
                     section 2 - In Progress
                 </section>
-                <section className='task_column_part'>
+                <section className={"task_column_part"}>
                     section 3 - Late Task
                 </section>
-                <section className='task_column_part'>
+                <section className={"task_column_part"}>
                     section 4 - Done
                 </section>
-                <section className='task_column_part'>
+                <section className={"task_column_part"}>
                     section 5 - Archive
                 </section>
             </main>
